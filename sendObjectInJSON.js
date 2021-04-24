54d68636132d9e70edef69f592b86cd4215b3a5d
SunDawning <dpmeichen@gmail.com>
2021-04-24 13:47:14 +0800

    print json in html

--- sendObjectInJSON.js
+++ sendObjectInJSON.js
@@ -4,17 +4,17 @@
 function sendObjectInJSON(url,method,object){
     let headers=new Headers();
     headers.set("content-type","application/json; charset=utf-8")
-    window.fetch(url,{
+    return window.fetch(url,{
         method:method,
         headers:headers,
         body:JSON.stringify(object),
     });
 }
 function postObjectInJSON(url,object){
-    sendObjectInJSON(url,"POST",object);
+    return sendObjectInJSON(url,"POST",object);
 }
 function putObjectInJSON(url,object){
-    sendObjectInJSON(url,"PUT",object);
+    return sendObjectInJSON(url,"PUT",object);
 }
 export{
     putObjectInJSON,
